b7510d1e422edb4982799ee0331f5f1dc0983f16
yoga <arilkiller07@gmail.com>
2026-09-07 20:30:30 +0700

    first commit

---
+++ latihan-fiber/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+"log"
+
+"github.com/gofiber/fiber/v3"
+)
+
+func main() {
+app := fiber.New()
+
+app.Get("/", func(c fiber.Ctx) error {
+		return c.SendString("Halo Pemrograman Web II")
+})
+
+app.Get("/api/info", func(c fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"aplikasi": "Latihan Fiber",
+			"versi":    "1.0.0",
+			"status":   "berjalan",
+		})
+})
+app.Get("/api/mahasiswa", func(c fiber.Ctx) error {
+    return c.JSON(fiber.Map{
+        "nim":   "Fachriel Yoga Wicaksono",
+        "nama":  "H1H024042",
+        "prodi": "TeknikKomputer",
+    })
+})
+
+log.Fatal(app.Listen(":3000"))
+}
---
+++ latihan-laravel/resources/views/HalamanDepan.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Halaman Depan</title>
+</head>
+<body>
+    <h1>Nama: Fachriel yoga WIcaksono</h1>
+    <p>NIM: H1H024042</p>
+</body>
+</html>
